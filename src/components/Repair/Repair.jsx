7caacef7706d7fdbf97d31c53b043fc763045e01
imater <eugene.leonar@gmail.com>
2017-11-04 01:32:15 +0500

    fix styles for sss

--- src/components/Repair/Repair.jsx
+++ src/components/Repair/Repair.jsx
@@ -1,23 +1,27 @@
-import React from 'react'
+import React, { PureComponent } from 'react'
 import Helmet from 'react-helmet'
 import styles from './Repair.sss'
 
-export default () => (
-  <div className={styles.repairContainer}>
-    <Helmet title='Технические работы' />
-    <div className={styles.repair}>
-      <span className={styles.version}>2.102</span>
-      <div className={styles.repairMessage}>
-        <h1 className={styles.h1}>
-          На сайте ведутся технические работы
-        </h1>
-        <h2 className={styles.h2}>
-          В настоящее время наши программисты делают для вас сайт еще лучше!
-        </h2>
+export default class Repair extends PureComponent {
+  render() {
+    return (
+      <div className={styles('repairContainer')}>
+        <Helmet title='Технические работы' />
+        <div className={styles('repair')}>
+          <span className={styles('version')}>2.102</span>
+          <div className={styles('repairMessage')}>
+            <h1 className={styles('h1')}>
+              На сайте ведутся технические работы.
+            </h1>
+            <h2 className={styles('h2')}>
+              В настоящее время наши программисты делают для вас сайт еще лучше!
+            </h2>
+          </div>
+          <div className={styles('copyright')}>
+            <span>4team, 2017</span>
+          </div>
+        </div>
       </div>
-      <div className={styles.copyright}>
-        <span>4team, 2017</span>
-      </div>
-    </div>
-  </div>
-)
+    )
+  }
+}
