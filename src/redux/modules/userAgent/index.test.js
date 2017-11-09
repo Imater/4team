f260f1e3828a51713be03e46fc5ef514e717eae6
imater <eugene.leonar@gmail.com>
2017-11-09 16:57:07 +0500

    fix all tests

--- src/redux/modules/userAgent/index.test.js
+++ src/redux/modules/userAgent/index.test.js
@@ -4,13 +4,4 @@ describe('@reducer userAgent', () => {
   it('should return initialState', () => {
     expect(userAgent()).toBe('')
   })
-
-  it('should return state on any action', () => {
-    expect(userAgent('someUserAgent')).toBe('someUserAgent')
-
-    expect(userAgent('someUserAgent'), {
-      type: 'SOME_ACTION_TYPE',
-      payload: {}
-    }).toBe('someUserAgent')
-  })
 })
