bb885136e47a007d89e33e7975b87756587b0e57
imater <eugene.leonar@gmail.com>
2017-11-09 23:04:48 +0500

    cleanup and fix lint

--- src/utils/renderLogger.js
+++
@@ -1,9 +0,0 @@
-import createLogger from 'react-render-logger'
-
-const enableChecker =
-  componentKey =>
-    componentKey
-
-const renderLogger = createLogger(enableChecker)
-
-export default renderLogger
