deeb8c1dd0fa5dfd2ee31c8a12e73b90114a2d6e
Igor Yarutkin <Igor.Yarutkin@csssr.com>
2017-11-11 16:58:07 +0300

    Make Box component

--- src/components/atoms/Box/Box.js
+++ src/components/atoms/Box/Box.js
@@ -1,23 +1,46 @@
 import React, { PureComponent } from 'react'
-import { node } from 'prop-types'
+import { number, string, bool, node } from 'prop-types'
+import cx from 'classnames'
 
 import styles from './Box.sss'
 
 export default class Box extends PureComponent {
   static propTypes = {
-    children: node
+    size: number,
+    color: string,
+    backgroundColor: string,
+    children: node,
+    isFirst: bool,
+    isLast: bool
   }
 
   static defaultProps = {
-
+    size: 36,
+    color: '#B1FF89',
+    backgroundColor: '#2A5D10',
+    isFirst: false,
+    isLast: false
   }
 
   render() {
-    // const {  } = this.props
+    const {
+      size,
+      color,
+      backgroundColor,
+      isFirst,
+      isLast,
+      children
+    } = this.props
 
     return (
-      <div className={styles.box}>
-        {this.props.children}
+      <div
+        className={cx(styles.box, {
+          [styles.box_isFirst]: isFirst,
+          [styles.box_isLast]: isLast
+        })}
+        style={{ width: size, minHeight: size, color, backgroundColor }}
+      >
+        {children}
       </div>
     )
   }
