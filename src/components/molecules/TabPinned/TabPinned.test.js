418e10c104f6710ed32c32f6b6e54155b79ffc70
Igor Yarutkin <Igor.Yarutkin@csssr.com>
2017-11-17 23:53:43 +0300

    Fix tests for rendering TabPinned with children

--- src/components/molecules/TabPinned/TabPinned.test.js
+++ src/components/molecules/TabPinned/TabPinned.test.js
@@ -1,12 +1,14 @@
 import React from 'react'
-import { shallow } from 'enzyme'
+import { shallow, mount } from 'enzyme'
 import TabPinned from '../TabPinned'
+import Box from '../../atoms/Box'
 
 describe('TabPinned', () => {
   it('should render normal without any props supplied', () => {
     expect(shallow(<TabPinned />)).toHaveLength(1)
   })
   it('should render normal with children content', () => {
-    expect(shallow(<TabPinned>TabPinned</TabPinned>).text()).toEqual('TabPinned')
+    const wrapper = mount(<TabPinned>TabPinned</TabPinned>)
+    expect(wrapper.find(Box).text()).toEqual('TabPinned')
   })
 })
