e5369b58f13961fba7f14f0d7c78cd6c21f9bc5b
Igor Yarutkin <Igor.Yarutkin@csssr.com>
2017-11-17 22:04:56 +0300

    Add complex example

--- src/components/molecules/TabPinned/TabPinned.stories.js
+++ src/components/molecules/TabPinned/TabPinned.stories.js
@@ -9,3 +9,17 @@ storiesOf('TabPinned', module)
   .addWithInfo('Disabled', () => (
     <TabPinned isDisabled />
   ))
+  .addWithInfo('Complex example', () => (
+    <div style={{ display: 'flex', alignItems: 'center' }}>
+      <div style={{ borderRight: '1px solid #000' }}>
+        <TabPinned color='#FFFFFF' backgroundColor='#FFEF09' icon='cloud' />
+      </div>
+      <div style={{ borderRight: '1px solid #000' }}>
+        <TabPinned isDisabled />
+      </div>
+      <div style={{ borderRight: '1px solid #000' }}>
+        <TabPinned color='#FFFFFF' backgroundColor='#FF00FE' icon='gift' isDisabled />
+      </div>
+      <TabPinned color='#FFFFFF' backgroundColor='#4443DC' icon='car' isDisabled />
+    </div>
+  ))
