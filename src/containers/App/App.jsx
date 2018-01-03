ddbb1939fe3b62188a8960eaa868b912ef518396
Rustam Yusupov <rustam.yusupov@gmail.com>
2018-01-03 19:21:01 +0200

    feat(variables, base, App): test bootstrap

--- src/containers/App/App.jsx
+++ src/containers/App/App.jsx
@@ -1,23 +1,18 @@
-import React, { Component, PropTypes as pt } from 'react'
+import React, { Component } from 'react'
 import pureRender from 'pure-render-decorator'
 import { connect } from 'react-redux'
 import { asyncConnect } from 'redux-async-connect'
+import { Button } from 'react-bootstrap'
 import styles from './App.styl'
 
 @asyncConnect([{ promise: () => Promise.resolve() }])
 @connect()
 @pureRender
 export default class App extends Component {
-  static propTypes = {
-    children: pt.any
-  }
-
   render() {
-    const { children } = this.props
-
     return (
       <div className={styles.app}>
-        {children}
+        <Button bsStyle='primary' bsSize='large'>Default</Button>
       </div>
     )
   }
