d25480dd67079272fc393f784d23944571b6e696
Rustam Yusupov <rustam.yusupov@gmail.com>
2018-01-04 20:19:12 +0200

    feat(App): add Split, Iframe

--- src/containers/App/App.jsx
+++ src/containers/App/App.jsx
@@ -2,7 +2,8 @@ import React, { Component } from 'react'
 import pureRender from 'pure-render-decorator'
 import { connect } from 'react-redux'
 import { asyncConnect } from 'redux-async-connect'
-import { Button } from 'react-bootstrap'
+import Split from 'components/Split'
+import Iframe from 'components/Iframe'
 import styles from './App.styl'
 
 @asyncConnect([{ promise: () => Promise.resolve() }])
@@ -12,7 +13,10 @@ export default class App extends Component {
   render() {
     return (
       <div className={styles.app}>
-        <Button bsStyle='primary' bsSize='large'>Default</Button>
+        <Split
+          left={<div>Left</div>}
+          right={<Iframe url='http://jira.relef.ru/browse/CSSSR-718' />}
+        />
       </div>
     )
   }
