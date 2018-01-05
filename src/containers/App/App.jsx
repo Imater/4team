19fdae7814df8d5f646f32fa3942f0044909ceb9
Rustam Yusupov <rustam.yusupov@gmail.com>
2018-01-05 20:32:31 +0200

    feat(App): add users panel

--- src/containers/App/App.jsx
+++ src/containers/App/App.jsx
@@ -4,17 +4,45 @@ import { connect } from 'react-redux'
 import { asyncConnect } from 'redux-async-connect'
 import Split from 'components/Split'
 import Iframe from 'components/Iframe'
+import LeftPanel from 'components/LeftPanel'
+import UserPanel from 'components/UserPanel'
 import styles from './App.styl'
+
+const users = [
+  {
+    name: 'Все'
+  }, {
+    name: 'Женя'
+  }, {
+    name: 'Олег'
+  }, {
+    name: 'Арарат'
+  }, {
+    name: 'Рустам'
+  }, {
+    name: 'Денис'
+  }, {
+    name: 'Андрей'
+  }
+]
 
 @asyncConnect([{ promise: () => Promise.resolve() }])
 @connect()
 @pureRender
 export default class App extends Component {
+  renderLeftPanel() {
+    return (
+      <LeftPanel>
+        <UserPanel users={users} />
+      </LeftPanel>
+    )
+  }
+
   render() {
     return (
       <div className={styles.app}>
         <Split
-          left={<div>Left</div>}
+          left={this.renderLeftPanel()}
           right={<Iframe url='http://jira.relef.ru/browse/CSSSR-718' />}
         />
       </div>
