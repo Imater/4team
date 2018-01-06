59cfab8ca3c9b2ce9f8511323f7b0cb37b7daf4b
Rustam Yusupov <rustam.yusupov@gmail.com>
2018-01-06 21:17:48 +0200

    feat(App): add workday with tasks

--- src/containers/App/App.jsx
+++ src/containers/App/App.jsx
@@ -6,6 +6,8 @@ import Split from 'components/Split'
 import Iframe from 'components/Iframe'
 import LeftPanel from 'components/LeftPanel'
 import UserPanel from 'components/UserPanel'
+import WorkDay from 'components/WorkDay'
+import Tasks from 'components/Tasks'
 import styles from './App.styl'
 
 const users = [
@@ -26,6 +28,35 @@ const users = [
   }
 ]
 
+const today = {
+  name: 'Сегодня',
+  time: '06:20:37'
+}
+
+const tasks = [
+  {
+    name: 'CSSSR-567 Верстка - мой прайс - возможность выгрузить данные',
+    url: 'http://jira.relef.ru/browse/CSSSR-567',
+    time: '01:41:31'
+  }, {
+    name: 'CSSSR-725 Доступ менеджера - ошибка отображения интерфейса (вкладка Партнеры)',
+    url: 'http://jira.relef.ru/browse/CSSSR-725',
+    time: '22:10'
+  }, {
+    name: 'CSSSR-723 [Релиз 27.12.2017] Некорректная начальная дата истории загрузок и продлений (сборка + исправление замечаний)',
+    url: 'http://jira.relef.ru/browse/CSSSR-723',
+    time: '02:12:58'
+  }, {
+    name: 'CSSSR-707 [Релиз 27.12.2017] Не работает функционал Цены на сайте на странице Ассортимент и остатки (меняем принцип округления)',
+    url: 'http://jira.relef.ru/browse/CSSSR-707',
+    time: '40:16'
+  }, {
+    name: 'CSSSR-732 Клиентская часть - Отображение остатков',
+    url: 'http://jira.relef.ru/browse/CSSSR-732',
+    time: '02:23:41'
+  }
+]
+
 @asyncConnect([{ promise: () => Promise.resolve() }])
 @connect()
 @pureRender
@@ -34,6 +65,10 @@ export default class App extends Component {
     return (
       <LeftPanel>
         <UserPanel users={users} />
+
+        <WorkDay day={today}>
+          <Tasks tasks={tasks} />
+        </WorkDay>
       </LeftPanel>
     )
   }
