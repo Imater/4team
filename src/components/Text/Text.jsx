b9be9e8ba1d9f8f38230cd3e033260815fea9dec
Rustam Yusupov <rustam.yusupov@gmail.com>
2018-01-06 20:37:59 +0200

    fix(Text): import styles

--- src/components/Text/Text.jsx
+++ src/components/Text/Text.jsx
@@ -1,4 +1,5 @@
 import React, { PureComponent, PropTypes as pt } from 'react'
+import cx from 'classnames'
 import styles from './Text.styl'
 
 export default class Text extends PureComponent {
@@ -30,22 +31,22 @@ export default class Text extends PureComponent {
 
     return (
       <p
-        className={styles('text', {
-          size_12: size === 12,
-          size_14: size === 14,
-          size_16: size === 16,
-          size_18: size === 18,
-          size_20: size === 20,
-          size_22: size === 22,
-          size_24: size === 24,
-          size_28: size === 28,
-          size_32: size === 32,
-          size_36: size === 36,
-          with_margin: margin,
-          mode_ellipsis: ellipsis,
-          align_center: align === 'center',
-          align_right: align === 'right',
-          align_justify: align === 'justify'
+        className={cx(styles.text, {
+          [styles.size_12]: size === 12,
+          [styles.size_14]: size === 14,
+          [styles.size_16]: size === 16,
+          [styles.size_18]: size === 18,
+          [styles.size_20]: size === 20,
+          [styles.size_22]: size === 22,
+          [styles.size_24]: size === 24,
+          [styles.size_28]: size === 28,
+          [styles.size_32]: size === 32,
+          [styles.size_36]: size === 36,
+          [styles.with_margin]: margin,
+          [styles.mode_ellipsis]: ellipsis,
+          [styles.align_center]: align === 'center',
+          [styles.align_right]: align === 'right',
+          [styles.align_justify]: align === 'justify'
         })}
       >
         {children}
