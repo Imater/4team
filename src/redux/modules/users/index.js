33ead1376463b0c8598be3e3a79f4a80229a31fe
Rustam Yusupov <rustam.yusupov@gmail.com>
2018-02-05 20:37:22 +0200

    feat(users): get filtered users

--- src/redux/modules/users/index.js
+++ src/redux/modules/users/index.js
@@ -2,6 +2,7 @@ import { createAction, createReducer } from 'redux-act'
 import { loop, Effects } from 'redux-loop'
 import axios from 'axios'
 import R from 'ramda'
+import config from 'config'
 
 const initialState = {
   isLoading: false,
@@ -34,12 +35,21 @@ const handleFetch = (state, { id, token }) =>
 
 const handleFetchSuccess = (state, payload) => {
   const data = R.path(['data'], payload)
+  const configUsers = config.users.reduce((acc, item) =>
+    [].concat(acc, item.email), [])
+  const filteredData = R.filter(user =>
+    R.contains(user.email, configUsers))(data)
+  const filteredUsers = R.map(R.pick(['id', 'name', 'email']))(filteredData)
+  const users = filteredUsers.map(user => ({
+    ...user,
+    name: user.name.split(' ')[0]
+  }))
 
   return {
     ...state,
     isLoading: false,
     isLoaded: true,
-    users: data,
+    users,
     error: ''
   }
 }
