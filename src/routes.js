3b21cdf34f2cb9d4702d353cee796cb55c6593b5
Rustam Yusupov <rustam.yusupov@gmail.com>
2018-02-17 20:41:09 +0200

    feat(routes): add user:id route

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,14 @@
 import App from './containers/App'
+import User from './containers/User'
 
 // export default routes
 export default () => ({
   path: '/',
-  component: App
+  component: App,
+  childRoutes: [
+    {
+      path: '/user/:id',
+      component: User
+    }
+  ]
 })
