7edf5570da7ff18d9e27a4a02f213f05fac351f0
Rustam Yusupov <rustam.yusupov@gmail.com>
2018-03-20 20:37:08 +0200

    feat(User): add active user link style

--- src/components/UserPanel/UserPanel.jsx
+++ src/components/UserPanel/UserPanel.jsx
@@ -1,5 +1,4 @@
 import React, { PureComponent, PropTypes as pt } from 'react'
-import { Glyphicon } from 'react-bootstrap'
 import { Link } from 'react-router'
 import Text from 'components/Text'
 
@@ -19,14 +18,14 @@ export default class UserPanel extends PureComponent {
       key={key}
       className={styles.user}
     >
-      <Link to={`/user/${id}`}>
-        <div className={styles.wrapper}>
-          <Glyphicon glyph='user' />
-
-          <Text size={16}>
-            {name}
-          </Text>
-        </div>
+      <Link
+        className={styles.link}
+        activeClassName={styles.link_active}
+        to={`/user/${id}`}
+      >
+        <Text size={16}>
+          {name}
+        </Text>
       </Link>
     </div>
   )
