09f4f67d4e0c4106d940e701dd69f7e89b047507
Rustam Yusupov <rustam.yusupov@gmail.com>
2018-04-03 22:19:24 +0200

    feat(users): fetch project users

--- src/redux/modules/users/index.js
+++ src/redux/modules/users/index.js
@@ -2,16 +2,21 @@ import { createAction, createReducer } from 'redux-act'
 import { loop, Effects } from 'redux-loop'
 import axios from 'axios'
 import R from 'ramda'
-import config from 'config'
 
 const initialState = {
   isLoading: false,
-  isLoaded: false
+  isLoaded: false,
+  isProjectUsersLoading: false,
+  isProjectUsersLoaded: false
 }
 
 export const fetch = createAction('users/FETCH')
 const fetchSuccess = createAction('users/FETCH_SUCCESS')
 const fetchFailure = createAction('users/FETCH_FAILURE')
+
+export const fetchProjectUsers = createAction('users/FETCH_PROJECT_USERS')
+const fetchProjectUsersSuccess = createAction('users/FETCH_PROJECT_USERS_SUCCESS')
+const fetchProjectUsersFailure = createAction('users/FETCH_PROJECT_USERS_FAILURE')
 
 const request = ({ id, token }) =>
   axios.get(`/api/v8/workspaces/${id}/workspace_users`, {
@@ -34,14 +39,10 @@ const handleFetch = (state, { id, token }) =>
   )
 
 const handleFetchSuccess = (state, payload) => {
-  const data = R.path(['data'], payload)
-  const configUsers = config.users.reduce((acc, item) =>
-    [].concat(acc, item.email), [])
-  const filteredData = R.filter(user =>
-    R.contains(user.email, configUsers))(data)
+  const data = R.prop('data', payload)
   const filteredFields =
-    R.map(R.pick(['id', 'name', 'email', 'uid']))(filteredData)
-  const users = filteredFields.map(user => ({
+    R.map(R.pick(['id', 'name', 'email', 'uid']))(data)
+  const items = filteredFields.map(user => ({
     ...user,
     name: user.name.split(' ')[0]
   }))
@@ -50,7 +51,7 @@ const handleFetchSuccess = (state, payload) => {
     ...state,
     isLoading: false,
     isLoaded: true,
-    users,
+    items,
     error: ''
   }
 }
@@ -62,10 +63,55 @@ const handleFetchFailure = (state, payload) => ({
   error: payload.message
 })
 
+
+const requestProjectUsers = ({ id, token }) =>
+  axios.get(`/api/v8/projects/${id}/project_users`, {
+    auth: {
+      username: token,
+      password: 'api_token'
+    }
+  })
+    .then(fetchProjectUsersSuccess)
+    .catch(fetchProjectUsersFailure)
+
+const handleFetchProjectUsers = (state, { id, token }) =>
+  loop(
+    {
+      ...state,
+      isProjectUsersLoading: true,
+      isProjectUsersLoaded: false
+    },
+    Effects.promise(requestProjectUsers, { id, token })
+  )
+
+const handleFetchProjectUsersSuccess = (state, payload) => {
+  const data = R.pathOr([], ['data'], payload)
+  const uids = R.map(user => user.uid)(data)
+  const users = state.items.filter(user => R.contains(user.uid, uids))
+
+  return {
+    ...state,
+    isProjectUsersLoading: false,
+    isProjectUsersLoaded: true,
+    users,
+    error: ''
+  }
+}
+
+const handleFetchProjectUsersFailure = (state, payload) => ({
+  ...state,
+  isProjectUsersLoading: false,
+  isProjectUsersLoaded: false,
+  error: payload.message
+})
+
 const reducer = createReducer(on => {
   on(fetch, handleFetch)
   on(fetchSuccess, handleFetchSuccess)
   on(fetchFailure, handleFetchFailure)
+  on(fetchProjectUsers, handleFetchProjectUsers)
+  on(fetchProjectUsersSuccess, handleFetchProjectUsersSuccess)
+  on(fetchProjectUsersFailure, handleFetchProjectUsersFailure)
 }, initialState)
 
 export default reducer
